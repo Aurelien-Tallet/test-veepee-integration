6040195a5500d7198eeefa613ea7b238c078a798
Aurelien Tallet <aurelien.jarte@gmail.com>
2020-12-15 17:47:55 +0100

    project's structure defined

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import React from 'react'
+import Header from 'components/Header/Header'
+import Footer from 'components/Footer/Footer'
+import PageContent from 'domain/Destinations/PageContent/PageContent'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
+      <PageContent />
+      <Footer />
 
-    </div>
-  );
+    </>
+  )
 }
-
-export default App;
---
+++ src/domain/Destinations/PageContent/PageContent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PageContent.scss'
+
+export default function PageContent() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
